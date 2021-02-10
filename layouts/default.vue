21d3722e15338e145d955ba82a25a3091db99fa1
張祐鈞 <boggyjan@baijideiMac.local>
2021-02-10 19:16:18 +0800

    新增ga代碼

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,5 +4,15 @@
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Varela+Round&display=swap" rel="stylesheet">
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-F10400YTQ5"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'G-F10400YTQ5');
+	</script>
   </div>
 </template>
