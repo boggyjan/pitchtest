3d144020b18d9adcb7a284cf2cab154b7b6e90f4
張祐鈞 <chouyuuhitoshi@baijide-MacBook-Pro.local>
2021-02-13 23:31:12 +0800

    重構分數API

--- middleware/score.js
+++ middleware/score.js
@@ -2,39 +2,31 @@ import fs from 'fs'
 import path from 'path'
 import querystring from 'querystring'
 
-export default async function (req, res, next) {
+export default function (req, res, next) {
   var filePath = path.join(__dirname, '../static/scoredata')
 
   if (process.env.VUE_ENV === 'server') {
     if (req.method === 'GET') {
-      // GET
-      fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-          res.writeHead(404, { 'Content-Type': 'application/json' })
-          res.end(JSON.stringify({
-            message: 'database error'
-          }))
-        } else {
+      // [GET] 取得分數列表
+      try {
+        let data = fs.readFileSync(filePath, 'utf8')
 
-          // 暫時解決有時候不知為何造成檔案結尾多一個中括弧 ]] 的問題
-          data = data.replace(/].*]/g, ']')
+        // 暫時解決多執行序同時寫入時造成json錯誤的問題
+        data = data.replace(/].*/g, ']')
 
-          // response
-          res.writeHead(200, { 'Content-Type': 'application/json' })
-          // return top 100
+        // return top 100
+        const list = JSON.parse(data).slice(0, 100)
 
-          try {
-            const list = JSON.parse(data).slice(0, 100)
-            res.end(JSON.stringify({ list }))
-          } catch (err) {
-            res.writeHead(500, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify({
-              message: 'data error'
-            }))
-            return
-          }
-        }
-      })
+        // response
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ list }))
+      } catch (err) {
+        res.writeHead(404, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({
+          message: 'database error'
+        }))
+      }
+
     } else {
       // POST
 
@@ -76,80 +68,62 @@ export default async function (req, res, next) {
 
           if (tokenIsAvailable) {
             // read record file
-            fs.readFile(filePath, 'utf8', (err, data) => {
-              if (err) {
-                res.writeHead(500, { 'Content-Type': 'application/json' })
-                res.end(JSON.stringify({
-                  message: 'database error'
-                }))
-              } else {
+            try {
+              let data = fs.readFileSync(filePath, 'utf8')
 
-                // 暫時解決有時候不知為何造成檔案結尾多一個中括弧 ]] 的問題
-                data = data.replace(/].*]/g, ']')
+              // 暫時解決多執行序同時寫入時造成json錯誤的問題
+              data = data.replace(/].*/g, ']')
 
-                // record info
-                let record = {
-                  token: token,
-                  name: postData.name,
-                  date: parseFloat(postData.date),
-                  score: parseFloat(postData.score),
-                  duration: parseFloat(postData.duration)
-                }
-
-                let records
-
-                try {
-                  records = JSON.parse(data)
-                } catch (err) {
-                  res.writeHead(500, { 'Content-Type': 'application/json' })
-                  res.end(JSON.stringify({
-                    message: 'database error'
-                  }))
-                  return
-                }
-                
-                let sameTokenRecordIdx = records.findIndex(item => item.token === token)
-
-                if (sameTokenRecordIdx > -1) {
-                  records[sameTokenRecordIdx] = record
-                } else {
-                  records.push(record)
-                }
-
-                // sort records
-                records.sort((a, b) => {
-                  if (a.score !== b.score) {
-                    // high -> low
-                    return b.score - a.score
-                  } else {
-                    // fast -> slow
-                    return a.duration - b.duration
-                  }
-                })
-
-                // slice top 100
-                // records = records.slice(0, 100)
-
-                // write record
-                fs.writeFile(filePath, JSON.stringify(records), (err, data) => {
-                  if (err) {
-                    res.writeHead(500, { 'Content-Type': 'application/json' })
-                    res.end(JSON.stringify({
-                      message: 'database error'
-                    }))
-                  } else {
-                    // response
-                    res.writeHead(200, { 'Content-Type': 'application/json' })
-                    res.end(JSON.stringify({
-                      // body: record,
-                      // data: data,
-                      // tokenIsAvailable
-                      success: tokenIsAvailable
-                    }))
-                  }
-                })
+              // record info
+              let record = {
+                token: token,
+                name: postData.name,
+                date: parseFloat(postData.date),
+                score: parseFloat(postData.score),
+                duration: parseFloat(postData.duration)
               }
-            })
+
+              let records = JSON.parse(data)
+
+              let sameTokenRecordIdx = records.findIndex(item => item.token === token)
+
+              if (sameTokenRecordIdx > -1) {
+                records[sameTokenRecordIdx] = record
+              } else {
+                records.push(record)
+              }
+
+              // sort records
+              records.sort((a, b) => {
+                if (a.score !== b.score) {
+                  // high -> low
+                  return b.score - a.score
+                } else {
+                  // fast -> slow
+                  return a.duration - b.duration
+                }
+              })
+
+              // slice top 100
+              // records = records.slice(0, 100)
+
+              // write record
+              fs.writeFileSync(filePath, JSON.stringify(records), { encoding: 'utf8', flag: 'w' })
+
+              // response
+              res.writeHead(200, { 'Content-Type': 'application/json' })
+              res.end(JSON.stringify({
+                // body: record,
+                // data: data,
+                // tokenIsAvailable
+                success: tokenIsAvailable
+              }))
+            } catch (err) {
+              res.writeHead(500, { 'Content-Type': 'application/json' })
+              res.end(JSON.stringify({
+                message: 'database error'
+              }))
+            }
           } else {
             res.writeHead(401, { 'Content-Type': 'application/json' })
             res.end(JSON.stringify({
